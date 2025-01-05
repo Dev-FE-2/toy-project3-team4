fb0a6cbcc61c597800f2dd8c4105a586731e87fc
gold <goldegg127@gmail.com>
2025-01-06 02:42:14 +0900

    feat: layout 디렉토리와 레이아웃 컴포넌트 생성 (#1)

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import { getPageTitle } from '../utils';
+import { NavBar } from '../components';
+import { default as S } from './Layout.styles';
+
+const Layout = () => {
+  const location = useLocation();
+
+  return (
+    <S.Layout>
+      <NavBar />
+      <main>
+        <section className="page-container">
+          <h1 className="page-title">{getPageTitle(location.pathname)}</h1>
+          <Outlet />
+        </section>
+      </main>
+    </S.Layout>
+  );
+};
+
+export default Layout;
