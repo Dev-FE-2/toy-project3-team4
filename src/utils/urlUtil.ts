a9ff64a6cf5f1ef3e37a5576d4468e8466cd7604
mgyang53 <mg960503@gmail.com>
2025-01-07 18:35:05 +0900

    remove: urlName 관련 상수, 유틸함수 제거 (#5)

--- src/utils/urlUtil.ts
+++
@@ -1,5 +0,0 @@
-import { URL, URLName } from '@/constant';
-
-export const getURL = (key: URLName) => URL[key];
-export const getURLLink = (key: URLName) => URL[key].link;
-export const getURLText = (key: URLName) => URL[key].text;
