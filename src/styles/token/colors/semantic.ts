9bff63252374c39b1796580cde05cd12006e2ab0
gold <goldegg127@gmail.com>
2025-01-08 12:14:41 +0900

    fix: 상수 네이밍 컨벤션 적용, 접미사를 COLOR로 수정 (#12)

--- src/styles/token/colors/semantic.ts
+++ src/styles/token/colors/semantic.ts
@@ -1,23 +1,23 @@
 import { scale } from './scale';
 
-const primaryValue = '#3E5371';
-const neutralValue = '#F6F9FC';
-const neutralVariantValue = '#CED4DA';
-const darkValue = '#081732';
-const grayLightValue = '#C2C6C9';
-const grayDarkValue = '#9CA3AF';
-const whiteValue = '#FFFFFF';
+const PRIMARY_COLOR = '#3E5371';
+const NEUTRAL_COLOR = '#F6F9FC';
+const NEUTRAL_VARIANT_COLOR = '#CED4DA';
+const DARK_COLOR = '#081732';
+const GRAY_LIGHT_COLOR = '#C2C6C9';
+const GRAY_DARK_COLOR = '#9CA3AF';
+const WHITE_COLOR = '#FFFFFF';
 
 const semantic = {
-  primary: primaryValue,
+  primary: PRIMARY_COLOR,
   secondary: '#A2C0DE',
   success: '#4CAF50',
   danger: '#DA626B',
   warning: scale.warning.s500,
-  neutral: neutralValue, // 배경, 보조 텍스트, 구분선
-  neutralVariant: neutralVariantValue, // 보조적인 배경, 비활성 상태 UI
-  info: grayDarkValue,
-  disabled: neutralVariantValue,
+  neutral: NEUTRAL_COLOR, // 주요 컨텐츠 배경, 보조 텍스트, 구분선
+  neutralVariant: NEUTRAL_VARIANT_COLOR, // 보조적인 배경, 비활성 상태 UI, 그림자 효과
+  info: GRAY_DARK_COLOR,
+  disabled: NEUTRAL_VARIANT_COLOR,
   hover: {
     primary: '#758AA7',
     secondary: '#C3D9EF',
@@ -26,23 +26,23 @@ const semantic = {
     warning: scale.warning.s400,
   },
   background: {
-    white: whiteValue,
-    light: neutralValue,
+    white: WHITE_COLOR,
+    light: NEUTRAL_COLOR,
     gray: '#F8F8F8',
-    dark: darkValue,
+    dark: DARK_COLOR,
   },
   text: {
-    white: whiteValue,
-    grayLight: grayLightValue,
-    grayDark: grayDarkValue,
-    dark: darkValue,
-    nav: primaryValue,
-    placeholder: grayLightValue,
-    disabled: grayDarkValue,
+    white: WHITE_COLOR,
+    grayLight: GRAY_LIGHT_COLOR,
+    grayDark: GRAY_DARK_COLOR,
+    dark: DARK_COLOR,
+    nav: PRIMARY_COLOR,
+    placeholder: GRAY_LIGHT_COLOR,
+    disabled: GRAY_DARK_COLOR,
   },
   border: {
-    light: neutralValue,
-    focus: primaryValue,
+    light: NEUTRAL_COLOR,
+    focus: PRIMARY_COLOR,
   },
 };
 
