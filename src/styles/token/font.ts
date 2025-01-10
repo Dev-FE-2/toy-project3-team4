02be78d9b91444a2dbecce3980e1a0a82188b611
gold <goldegg127@gmail.com>
2025-01-10 16:44:15 +0900

    fix: font 디자인 토큰 업데이트 (#32)

--- src/styles/token/font.ts
+++ src/styles/token/font.ts
@@ -1,14 +1,13 @@
 export const font = {
   size: {
-    heading: '3.2rem',
-    headingLight: '2.8rem',
-    subHeading: '2.4rem',
+    heading: '2.4rem',
+    subHeading: '2.0rem',
     paragraph: '1.6rem',
     info: '1.4rem',
-    min: '1.3rem',
+    min: '1.4rem',
     values: {
-      heading: 36,
-      subHeading: 24,
+      heading: 24,
+      subHeading: 20,
       paragraph: 16,
       info: 14,
       min: 13,
@@ -16,10 +15,7 @@ export const font = {
   },
   family: "'Noto Sans KR', sans-serif",
   weight: {
-    heading: 500,
-    headingLight: 300,
-    light: 300,
-    normal: 400,
-    bold: 500,
+    heading: 600,
+    subHeading: 500,
   },
 };
