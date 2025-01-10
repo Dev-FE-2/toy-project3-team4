b52c542343d787abf8072262f5ee91e8c6d63539
gold <goldegg127@gmail.com>
2025-01-10 17:45:57 +0900

    settings: stylelint 설치 및 셋팅 (#29)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import stylelint from 'vite-plugin-stylelint';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), stylelint({})],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
