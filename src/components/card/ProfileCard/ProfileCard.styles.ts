711a5c2964b049bd4e26af4e457bd212fe87550c
Vastia <92559779+laivastia@users.noreply.github.com>
2025-01-13 12:32:20 +0900

    feat: 프로필 페이지에 사용자 정보 렌더링을 위한 컴포넌트 구현 (#21) (#42)

---
+++ src/components/card/ProfileCard/ProfileCard.styles.ts
@@ -0,0 +1,53 @@
+import { font, padding } from '@/styles';
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+  width: 100%;
+  max-width: 76rem;
+  padding: 0 ${padding['md']};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: ${padding['md']};
+  transition: 0.3s;
+
+  @media (width <= 768px) {
+    padding-top: ${padding.xl};
+  }
+`;
+
+export const CardImage = styled.img`
+  width: 10rem;
+  height: 10rem;
+`;
+
+export const CardWrapper = styled.div`
+  flex: 1 0 auto;
+`;
+
+export const CardTitle = styled.h3`
+  font-size: ${font.size['heading']};
+  font-weight: ${font.weight['heading']};
+  margin-bottom: 1.6rem;
+`;
+
+export const CardInfoBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CardStack = styled.button`
+  cursor: pointer;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const B = styled.b`
+  font-weight: ${font.weight['heading']};
+`;
