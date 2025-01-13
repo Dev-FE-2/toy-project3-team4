6d436531c8bf9b399f9e8f6d3be044d93f51013c
Vastia <92559779+laivastia@users.noreply.github.com>
2025-01-13 12:32:41 +0900

    feat: 검색, 프로필 페이지에서 무한 로딩에 사용 될 플레이 리스트 카드 컴포넌트 마크업 (#43) (#44)

---
+++ src/components/card/ExplorePlayListCard/ExplorePlayListCard.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/core';
+import { RiMore2Fill } from 'react-icons/ri';
+import * as S from './ExplorePlayListCard.styles';
+
+const ExplorePlayListCard = () => {
+  return (
+    <S.CardContainer>
+      <S.CardImageBox>
+        <S.CardImage src="src/assets/testThumbnail.png" />
+      </S.CardImageBox>
+      <S.CardInfoBox>
+        <h3>고양이는 너무 귀여웡</h3>
+        <S.CardTagBox>
+          <span>#고양</span>
+          <span>#고영</span>
+          <span>#냐옹이</span>
+        </S.CardTagBox>
+        <S.CardInfoLine>
+          <span>댓글 1천· 좋아요 1만</span>
+          <Button $bgColor="white" $size="xs">
+            <RiMore2Fill />
+          </Button>
+        </S.CardInfoLine>
+      </S.CardInfoBox>
+    </S.CardContainer>
+  );
+};
+
+export default ExplorePlayListCard;
---
+++ src/components/card/ExplorePlayListCard/index.ts
@@ -0,0 +1,1 @@
+export { default as ExplorePlayListCard } from './ExplorePlayListCard';
