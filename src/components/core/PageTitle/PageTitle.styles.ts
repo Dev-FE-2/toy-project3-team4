23c02b8b4a3459f65669a2e1a57ba02564f02ce4
mgyang53 <mg960503@gmail.com>
2025-01-14 23:09:06 +0900

    feat: 공통 페이지 제목 컴포넌트 추가 (#51)

---
+++ src/components/core/PageTitle/PageTitle.styles.ts
@@ -0,0 +1,7 @@
+import { font } from '@/styles';
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: ${font.size.subHeading};
+  font-weight: ${font.weight.subHeading};
+`;
