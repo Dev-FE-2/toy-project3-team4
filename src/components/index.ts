288a43fcf50f284085212a4fe1037d63da237de4
Vastia <92559779+laivastia@users.noreply.github.com>
2025-01-14 14:34:47 +0900

    [FIX] 디자인 시스템 변경에 의한 컬러 토큰값 변경 (#55)
    * fix: 디자인 시스템 변경에 의한 컬러 토큰값 변경 (#54)
    
    * fix: 디자인 토큰 변경에 의한 공용 버튼 컴포넌트 수정과 컴포넌트를 기능별로 모아두는 컨벤션 적용 (#54)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export * from './core';
 export * from './layout';
-export { GoogleLoginButton } from './signin';
+export * from './button';
 export { UserCard, ProfileCard, ExplorePlayListCard } from './card';
