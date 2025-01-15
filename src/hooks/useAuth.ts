c17817a2a1f3a828752a4f9b255ade80c8d09aaa
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2025-01-15 16:59:15 +0900

    [🔨 Fix] 로그인 후 유저정보 persist 설정 (#67)
    * fix: user 전역상태 persist 추가 (#64)
    
    * fix: 필요없어진 firebase auth 감지 삭제에 따른 수정 (#64)
    
    * fix: 제거한 커스텀훅 경로 삭제 (#64)

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,24 +1,24 @@
+import { FirebaseError } from 'firebase/app';
 import {
   GoogleAuthProvider,
   signInWithPopup,
   setPersistence,
   browserLocalPersistence,
 } from 'firebase/auth';
-import { AUTH } from '@/api/firebase';
+import { AUTH, addUser, getUser } from '@/api';
 import { useUserActions } from '@/store';
-import { addUser, getUser } from '@/api';
-import type { IUserAPISchema } from '@/types';
-import { useNavigate } from 'react-router-dom';
-import { formatUserResponseToState } from '@/utils';
 import { defaultUserSchema } from '@/constant';
+import { formatUserResponseToState } from '@/utils';
+import type { IUserAPISchema } from '@/types';
 
 const useAuth = () => {
-  const { setUser } = useUserActions();
-  const navigate = useNavigate();
+  const { setUser, clearUser } = useUserActions();
 
   // Google 로그인
   const loginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
+    const userCloseErrorCode = 'auth/popup-closed-by-user';
+
     try {
       await setPersistence(AUTH, browserLocalPersistence); // 세션 유지 설정
       const result = await signInWithPopup(AUTH, provider);
@@ -40,18 +40,24 @@ const useAuth = () => {
         // DB에 신규 유저 등록
         await addUser(userData as IUserAPISchema);
       }
-
       // 전역 상태에 유저 정보 저장
       setUser(formatUserResponseToState({ ...userData, userSn: uid }));
-      navigate('/'); // 메인 페이지로 이동
     } catch (error) {
       console.error('로그인 실패:', error);
+
+      // 사용자가 로그인 팝업창을 닫은 경우를 제외하고 오류 alert
+      if (
+        !(error instanceof FirebaseError && error.code === userCloseErrorCode)
+      ) {
+        alert('로그인에 실패하였습니다. 다시 시도해주세요.');
+      }
     }
   };
 
   // 로그아웃
   const logout = async () => {
     await AUTH.signOut();
+    clearUser();
     alert('로그아웃 되었습니다.');
   };
 
