1ff7017b49efe940af567d1f8ac470970c4950f8
Vastia <92559779+laivastia@users.noreply.github.com>
2025-01-15 22:48:05 +0900

    [FEAT] 사용자 정보 수정 페이지 개발 및 추가 요청 사항 구현 (#70)
    * feat: 원형의 이미지 출력 위한 공용 컴포넌트 개발 (#48)
    
    * feat: 내 정보 수정 페이지 마크업 (#48)
    
    * feat: tanstack-query 기본 설정 선언 및 queryProvider 적용 (#48)
    
    * feat: 사용자 정보 수정을 위한 api 개발 (#48)
    
    * feat: 사용자 정보 수정 관련 데이터 처리를 위한 커스텀 훅 개발 (#48)
    
    * feat: 사용자 정보 수정 페이지 구현 (#48)

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,6 +1,7 @@
 // atom(버튼, 인풋), 공용 기능 컴포넌트(탭, 아코디언, 드롭다운...)
 export { ProfileImage } from './ProfileImage';
 export { LoaderWrapper } from './Loader';
+export { RoundImage } from './RoundImage';
 export { PageTitle } from './PageTitle';
 export { Input } from './Input';
 export { Textarea } from './Textarea';
