a523dc121a9e2d386e44eff079d9e4c9a83fe488
gold <goldegg127@gmail.com>
2025-01-22 01:02:14 +0900

    feat: 쿼리 파라미터 상수 모듈 생성 (#106)

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,6 +3,7 @@ export * from './userSchema';
 export * from './dbCollection';
 export * from './navItems';
 export * from './interests';
-export * from './query';
+export * from './reactQuery';
 export * from './playlistLimits';
 export * from './iconSize';
+export * from './queryParams';
