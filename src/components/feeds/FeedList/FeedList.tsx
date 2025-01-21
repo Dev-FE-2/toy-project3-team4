e14dc2cce04ea73ca0422788c4455a6663638ed2
gold <goldegg127@gmail.com>
2025-01-21 19:39:48 +0900

    chore: 불필요한 코드 제거 (#106)

--- src/components/feeds/FeedList/FeedList.tsx
+++ src/components/feeds/FeedList/FeedList.tsx
@@ -1,17 +1,10 @@
-import { useContext } from 'react';
 import { useFetchAllPlaylist } from '@/hooks';
-import { FeedListContext } from '@/context';
 import { FeedListProvider } from '@/provider';
 import { LoaderWrapper, Feed } from '@/components';
 
 const FeedList = () => {
-  const context = useContext(FeedListContext);
   const { data: feedList, isLoading, isError, error } = useFetchAllPlaylist();
 
-  if (!context)
-    throw new Error(
-      'FeedListContext가 설정되지 않았습니다. 해당 컴포넌트가 FeedListProvider로 감싸져 있는지 확인하세요.',
-    );
   if (isError) return <div>오류 발생: {error?.message}</div>;
 
   return (
