04d78fb7f5f73007afc44ef9aee640390c5e5367
mgyang53 <mg960503@gmail.com>
2025-01-22 22:48:30 +0900

    fix: videoservice 수정 (#97)

--- src/api/external/video/videoService.ts
+++ src/api/external/video/videoService.ts
@@ -1,30 +1,33 @@
 import { formatISODurationToHHMMSS, formatSecondsToHHMMSS } from '@/utils';
 import type { Video } from '@/types';
+import defaultImage from '@/assets/avatar.svg';
 
-export const fetchYouTubeInfo = async (url: string): Promise<Video> => {
-  const videoId = url.includes('v=')
-    ? url.split('v=')[1]?.split('&')[0]
-    : url.split('.be/')[1]?.split('?')[0]; // 유튜브 영상 ID 추출
+type VideoInfo = Omit<Video, 'url'>;
+
+export const fetchYouTubeInfo = async (videoId: string): Promise<VideoInfo> => {
   const apiKey = import.meta.env.VITE_APP_GOOGLE_API_KEY; // YouTube API 키를 입력
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet,contentDetails`,
   ).then((res) => res.json());
 
-  const { title, description, thumbnails } = response.items[0].snippet;
+  const { title, description, thumbnails, channelTitle, publishedAt } =
+    response.items[0].snippet;
   const { duration } = response.items[0].contentDetails;
 
   return {
-    url,
+    videoId,
     title,
     description,
-    duration: formatISODurationToHHMMSS(duration),
     thumbnail: thumbnails.high.url,
+    author: channelTitle,
+    authorImgUrl: defaultImage, // 유튜브 채널 이미지는 부가적으로 api 호출해야 해서 임시로 defaultImage로 대체
+    publishedAt,
+    duration: formatISODurationToHHMMSS(duration),
     platform: 'youtube',
   };
 };
 
-export const fetchVimeoInfo = async (url: string): Promise<Video> => {
-  const videoId = url.split('/').pop(); // Vimeo 영상 ID 추출
+export const fetchVimeoInfo = async (videoId: string): Promise<VideoInfo> => {
   const response = await fetch(
     `https://vimeo.com/api/v2/video/${videoId}.json`,
   ).then((res) => res.json());
@@ -32,11 +35,14 @@ export const fetchVimeoInfo = async (url: string): Promise<Video> => {
   const data = response[0];
 
   return {
-    url,
+    videoId,
     title: data.title,
     description: data.description,
     duration: formatSecondsToHHMMSS(data.duration),
     thumbnail: data.thumbnail_large,
+    author: data.user_name,
+    authorImgUrl: data.user_portrait_huge || defaultImage,
+    publishedAt: data.upload_date,
     platform: 'vimeo',
   };
 };
