7c01aa1d422b62b57677a3958666ba421523f361
laivastia <zmfwl123@gmail.com>
2025-01-22 11:13:09 +0900

    feat: tanstack-query를 사용하여 무한 스크롤 데이터 패칭을 위한 커스텀 훅을 구현합니다. (#110)

--- src/hooks/playlist/index.ts
+++ src/hooks/playlist/index.ts
@@ -4,3 +4,4 @@ export { default as useFetchAllPlaylist } from './useFetchAllPlaylist';
 export { default as useUpdatePlaylist } from './useUpdatePlaylist';
 export { default as useDeletePlaylist } from './useDeletePlaylist';
 export { default as useFetchAuthor } from './useFetchAuthor';
+export { default as useSearchPlaylist } from './useSearchPlaylist';
