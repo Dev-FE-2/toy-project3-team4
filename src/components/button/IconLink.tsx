5d307bb92dd5f22bdcad0a6dce33d4c758b709fb
gold <goldegg127@gmail.com>
2025-01-22 15:14:44 +0900

    refactor: 공통 iconStyle 생성 및 적용 (#116)

--- src/components/button/IconLink.tsx
+++ src/components/button/IconLink.tsx
@@ -1,13 +1,9 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { iconStyle } from '@/styles';
 
 const IconLink = styled(Link)`
-  cursor: pointer;
-  transition: all 0.3s;
-
-  &:hover {
-    opacity: 0.7;
-  }
+  ${iconStyle}
 `;
 
 export default IconLink;
---
+++ src/styles/iconStyle.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const iconStyle = css`
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export default iconStyle;
