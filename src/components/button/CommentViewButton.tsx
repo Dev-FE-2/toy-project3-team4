6d1b8af87f70c368a20921835b47a7d0f1b43196
mgyang53 <mg960503@gmail.com>
2025-01-22 22:52:33 +0900

    fix: 아이콘링크 아닌 버튼인 경우도 추가 (#97)

--- src/components/button/CommentViewButton.tsx
+++ src/components/button/CommentViewButton.tsx
@@ -1,14 +1,26 @@
 import { RiChat1Line } from 'react-icons/ri';
 import { FEED_ICON_SIZE } from '@/constant';
-import { IconLink } from '@/components';
+import { IconButton, IconLink } from '@/components';
 
 interface ICommentViewButton {
-  link: string;
+  link?: string;
+  onCommentView?: () => void;
 }
 
-const CommentViewButton = ({ link }: ICommentViewButton) => {
+const CommentViewButton = ({ link, onCommentView }: ICommentViewButton) => {
+  if (onCommentView) {
+    return (
+      <IconButton
+        onClick={onCommentView}
+        aria-label="플레이리스트 재생 목록 보기"
+      >
+        <RiChat1Line size={FEED_ICON_SIZE} />
+      </IconButton>
+    );
+  }
+
   return (
-    <IconLink to={link} aria-label="플레이리스트 댓글 보기">
+    <IconLink to={link!} aria-label="플레이리스트 댓글 보기">
       <RiChat1Line size={FEED_ICON_SIZE} />
     </IconLink>
   );
