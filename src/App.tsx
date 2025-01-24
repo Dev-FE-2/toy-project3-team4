c7cd560c930798ab1986c69bb55acf29898ec928
gold <goldegg127@gmail.com>
2025-01-24 11:42:52 +0900

    settings: @tanstack/react-query-devtools 설치 및 설정 (#128)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppRoutes from '@/routes/AppRoutes';
 import GlobalStyle from './GlobalStyle';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/provider';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <AppRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
