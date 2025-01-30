d22716d16453887f97d11bb62ebe6a1d9eddc026
gold <goldegg127@gmail.com>
2025-01-31 04:27:54 +0900

    feat: 북마크와 좋아요를 업데이트할 때 캐시데이터도 업데이트 되도록 setQueryData 메서드 적용 (#118)

--- src/hooks/playlist/useUpdateLikes.ts
+++ src/hooks/playlist/useUpdateLikes.ts
@@ -1,8 +1,10 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updatePlaylist } from '@/api';
 import { useContextFeed } from '@/hooks';
+import type { IPlaylistAPISchema } from '@/types';
 
 const useUpdateLikes = (playlistSn: string) => {
+  const queryClient = useQueryClient();
   const { updateLikes: updateLikesContext } = useContextFeed(playlistSn);
 
   return useMutation<void, Error, { playlistSn: string; newLikes: string[] }>({
@@ -10,6 +12,20 @@ const useUpdateLikes = (playlistSn: string) => {
       if (!playlistSn || !newLikes) throw new Error('Invalid update data');
       await updatePlaylist(playlistSn, { likes: newLikes });
       updateLikesContext(playlistSn, newLikes);
+    },
+    onSuccess: (_, { playlistSn, newLikes }) => {
+      queryClient.setQueryData(
+        ['playlists'],
+        (prevData: IPlaylistAPISchema[]) => {
+          if (!prevData) return prevData;
+
+          return prevData.map((playlist) =>
+            playlist.playlistSn === playlistSn
+              ? { ...playlist, likes: newLikes }
+              : playlist,
+          );
+        },
+      );
     },
     onError: (error: Error) => {
       console.error('좋아요 업데이트 실패:', error.message);
--- src/hooks/playlist/useUpdateMyBookmarks.ts
+++ src/hooks/playlist/useUpdateMyBookmarks.ts
@@ -1,10 +1,12 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateUser } from '@/api';
 import { useUserSn } from '@/store';
 import { useContextFeed } from '@/hooks';
+import type { IUserAPISchema } from '@/types';
 
 const useUpdateMyBookmarks = (playlistSn: string) => {
   const userSn = useUserSn();
+  const queryClient = useQueryClient();
   const { updateMyBookmarks: updateBookmarksContext } =
     useContextFeed(playlistSn);
 
@@ -13,6 +15,16 @@ const useUpdateMyBookmarks = (playlistSn: string) => {
       if (!userSn || !newBookmarks) throw new Error('Invalid update data');
       await updateUser(userSn, { bookmarks: newBookmarks });
       updateBookmarksContext(newBookmarks);
+    },
+    onSuccess(_, { newBookmarks }) {
+      queryClient.setQueryData(['myInfo'], (prevData: IUserAPISchema[]) => {
+        if (!prevData) return prevData;
+
+        return {
+          ...prevData,
+          bookmarks: newBookmarks,
+        };
+      });
     },
     onError: (error: Error) => {
       console.error('북마크 업데이트 실패:', error.message);
