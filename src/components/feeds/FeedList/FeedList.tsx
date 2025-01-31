5a9ff052bba729bbda7b70fd6a1d5386f8f50944
gold <goldegg127@gmail.com>
2025-01-31 14:26:57 +0900

    feat: 메인 피드리스트 최신순 정렬 (#118)

--- src/components/feeds/FeedList/FeedList.tsx
+++ src/components/feeds/FeedList/FeedList.tsx
@@ -19,14 +19,20 @@ const FeedList = () => {
   if (isFeedError) return <div>오류 발생: {feedError?.message}</div>;
   if (isMyInfoError) return <div>오류 발생: {myInfoError?.message}</div>;
 
+  const latestFeedList = feedList
+    ? [...feedList].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+      )
+    : [];
+
   return (
     <LoaderWrapper
       isLoading={isFeedLoading || isMyInfoLoading}
       text="로딩 중..."
     >
       <ul>
-        {feedList && feedList.length > 0 ? (
-          feedList.map((feed, index) => (
+        {latestFeedList && latestFeedList.length > 0 ? (
+          latestFeedList.map((feed, index) => (
             <li key={`${index}-${feed.playlistSn}`}>
               <Feed feed={feed} myBookmarks={myInfo?.bookmarks || []} />
             </li>
